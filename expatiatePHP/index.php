63778db3583fd96eb8b1dcd9f612334ef847b8f8
BetterFeng <BetterFeng@BetterFeng-PC>
2015-05-07 13:12:07 +0800

    配置PHP练习项目

---
+++ expatiatePHP/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Filename: index.php
+ * Encoding: UTF-8
+ * Author: BetterFeng
+ * Link: http://www.fszqit.com
+ * Copyright: Copyright (c)2015-2005 佛山市昭群移动应用工作室 All rights Reserved. 
+ * Datetime: 2015-5-7  13:09:04
+ * Version: 1.0
+ * Description:
+ */
+echo "Hello World!";
+?>
+
