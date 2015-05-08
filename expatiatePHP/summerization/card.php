40472c9cfc64d19503b6f33baf0793b3aa57ce01
BetterFeng <BetterFeng@BetterFeng-PC>
2015-05-08 21:56:19 +0800

    增加概要

---
+++ expatiatePHP/summerization/card.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+  Filename: card.php
+  Encoding: UTF-8
+  Author: BetterFeng
+  Link: http://www.fszqit.com
+  Copyright: Copyright (c)2015-2005 佛山市昭群移动应用工作室 All rights Reserved. 
+  Datetime: 2015-5-8  21:32:08
+  Version: 1.0
+  Description:
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
