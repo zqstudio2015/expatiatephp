3ca203248e8adca53a5bca5b0b4d1d53766cde65
Better <Better@BetterVBox-PC>
2015-05-21 20:08:33 +0800

    细说PHP HTML 面向对象2

--- expatiatePHP/interfaceofphp/class_form.php
+++ expatiatePHP/interfaceofphp/class_form.php
@@ -15,16 +15,60 @@
  * @author Better
  */
 class form {
+
     private $form_name;
     private $request;
     private $action;
     private $method;
     private $target;
-    
-    function __construct() {
-        $str = "<form action =".$this->action."method=".$this->method."target=".$this->target.">";
-        $str.="<table align=center border=0 width=400>";
-        $str.="<caption><h3>".$this->form_name."</h3></caption>";
-        
+
+    function __construct($form_name, $request, $action = "index.php", $method = "get", $target = "_self") {
+        $this->form_name = $form_name;
+        $this->request = $request;
+        $this->action = $action;
+        $this->method = $method;
+        $this->target = $target;
     }
+
+    function __toString() {
+//        $str = '<form action ='.$this->action.' method='.$this->method.' target='.$this->target.'>';
+//        $str.='<table align=center border=0 width=400>';
+        $str = '<table align=center border=0 width=400>';
+        $str .= '<caption><h3>' . $this->form_name . "</h3></caption>";
+        $str .= '<form action =' . $this->action . ' method=' . $this->method . ' target=' . $this->target . '>';
+        switch ($this->request["action"]) {
+            case 1:
+                $str.='<tr><th>矩形高度：</th><td>';
+                $str.='<input type="text" name="height" value=' . $this->request["height"] . '>';
+                $str.='</td></tr>';
+                $str.='<tr><th>矩形宽度：</th><td>';
+                $str.='<input type="text" name="width" value=' . $this->request["width"] . '>';
+                $str.='</td></tr>';
+                break;
+            case 2:
+                $str.='<tr><th>第一条边：</th><td>';
+                $str.='<input type="text" name="side1" value=' . $this->request["side1"] . '>';
+                $str.='</td></tr>';
+                $str.='<tr><th>第二条边：</th><td>';
+                $str.='<input type="text" name="side2" value=' . $this->request["side2"] . '>';
+                $str.='</td></tr>';
+                $str.='<tr><th>第三条边：</th><td>';
+                $str.='<input type="text" name="side3" value=' . $this->request["side3"] . '>';
+                $str.='</td></tr>';
+                break;
+            case 3:
+                $str.='<tr><th>圆的半径：</th><td>';
+                $str.='<input type="text" name="radius" value=' . $this->request["radius"] . '>';
+                $str.='</td></tr>';
+        }
+        $str .= '<tr><td align=center colspan=2><input type="submit" value="计算"></td></tr>';
+        $str .= '<input type="hidden" name="act" value=' . $this->request["action"] . '>';
+        $str .= '<input type="hidden" name="action" value=' . $this->request["action"] . '>';
+        $str .= '</form>';
+        $str .= '</table>';
+//        $str.='</form>';
+
+        return $str;
+    }
+
 }
---
+++ expatiatePHP/interfaceofphp/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--
+文件名： index.php
+字符编码：UTF-8  
+版权所有: Copyright©2014-2015 Zhao Qun Studio Co.,Ltd
+网站地址:http://zqstudio.ecip.net
+作者:Better Feng
+邮箱:2360680821@qq.com
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>图形的周长和面积计算器</title>
+    </head>
+    <body>
+        <?php
+
+        function __autoload($classname) {
+            include("class_" .($classname). ".php");
+        }
+        ?>
+    <center>
+        <h2>图形的周长和面积计算器</h2>
+        <hr>
+        <a href="index.php?action=1">矩形</a> ||
+        <a href="index.php?action=2">三角形</a> ||
+        <a href="index.php?action=3">圆形</a><hr>
+    </center>
+
+    <?php
+    switch ($_REQUEST["action"]) {
+        case 1:
+            $form = new form("矩形", $_REQUEST, "index.php");
+            echo $form;
+            break;
+        case 2:
+            $form = new form("三角形", $_REQUEST, "index.php", "post", "_blank");
+            echo $form;
+            break;
+        case 3:
+            $form = new form("圆形", $_REQUEST);
+            echo $form;
+            break;
+        default:
+            echo "请选择一个形状<br>";
+    }
+    
+    if(isset($_REQUEST["act"])){
+        switch ($_REQUEST["act"]){
+            case 1:
+                $shape = new rect($_REQUEST);
+                break;
+            case 2:
+                $shape = new triangle($_REQUEST);
+                break;
+            case 3:
+                $shape = new circle($_REQUEST);
+                break;
+        }
+        echo "面积为：".$shape->area().'<br>';
+        echo "周长为：".$shape->perimeter().'<br>';
+    }
+    
+    ?>
+</body>
+</html>
