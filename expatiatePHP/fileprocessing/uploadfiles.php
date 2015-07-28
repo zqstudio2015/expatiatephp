9a34d6a93a9c92f4c6b7be410abf591024df083b
Better <Better@BetterVBox-PC>
2015-07-28 11:24:43 +0800

    细说PHP 文件管理7 上传多个文件1

---
+++ expatiatePHP/fileprocessing/uploadfiles.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+文件名： uploadfile.php
+字符编码：UTF-8  
+版权所有: Copyright©2014-2015 Zhao Qun Studio Co.,Ltd
+网站地址:http://zqstudio.ecip.net
+作者:Better Feng
+邮箱:2360680821@qq.com
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>多个文件上传</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <form action="mul_upload.php" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="MAX_FILE_SIZE" value="1000000">
+            选择文件1:<input type="file" name="myfile[]">
+            选择文件2:<input type="file" name="myfile[]">
+            选择文件3:<input type="file" name="myfile[]">
+            <input type="submit" value="上传文件">
+        </form>
+    </body>
+</html>
