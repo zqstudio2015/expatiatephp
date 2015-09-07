8f05259762c356cebf787d587a4e521f968561b6
Better <Better@BetterVBox-PC>
2015-09-07 14:40:13 +0800

    细说PHP 文件文件管理 添加contral.php

---
+++ expatiatePHP/filesystem/contral.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * 文件名： contral.php
+ * 字符编码：UTF-8  
+ * 版权所有: Copyright©2014-2015 Zhao Qun Studio Co.,Ltd
+ * 网站地址:http://zqstudio.ecip.net
+ * 作者:Better Feng
+ * 邮箱:2360680821@qq.com
+ */
+
+function __autoload($className){
+    include $className . "_class.php";
+}
+
+isset($_GET["action"]) or die("没有任何活动发生");
+
+$fileaction = new FileAction($_GET["filename"], $_GET["action"]);
+$fileaction->getFileInfo();
+
+if(isset($_GET["dirname"])){
+    $fileaction->getForm("filesystem.php?dirname=" . $_GET["action"]);
+} else {
+    $fileaction->getForm("filesystem.php");
+}
+
+?>
+
