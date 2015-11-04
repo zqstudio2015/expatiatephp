a31e83d778973c7d0388d9abcc14e27e1412b57f
Better <Better@BetterVBox-PC>
2015-11-04 17:20:32 +0800

    include:读取评估； require:替换 都是包括文件

---
+++ expatiatePHP/testphpapi/includeB.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * 文件名： includeB.php
+ * 字符编码：UTF-8  
+ * 版权所有: Copyright©2014-2015 Zhao Qun Studio Co.,Ltd
+ * 网站地址:http://zqstudio.ecip.net
+ * 作者:Better Feng
+ * 邮箱:2360680821@qq.com
+ */
+
+/**
+ * Description of includeB
+ *
+ * @author Better
+ */
+echo "includeB"."<br>";
+?>
